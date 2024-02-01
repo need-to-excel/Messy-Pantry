7fdb1b9afb4b1f1af3fcdd656c5390cfb63aa743
Betelgeuse <abdumavlyanov2004@yahoo.com>
2024-02-01 21:17:21 +0000

    api tested

---
+++ script.js
@@ -0,0 +1,12 @@
+var apiKey = "0qKmN4G3IDKPTq3BC91TZXoTnlsKdCl0"
+    
+var queryURL = "https://api.giphy.com/v1/gifs/random?tag=wine&api_key=" + "0qKmN4G3IDKPTq3BC91TZXoTnlsKdCl0";
+  fetch(queryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data.bitly_gif_url);
+    //   var url = (data.images);
+    //   console.log(url)
+});
