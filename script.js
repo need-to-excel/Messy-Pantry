ddb09356f5f75b8c38441f8e45d80967f8ec03b5
Betelgeuse <abdumavlyanov2004@yahoo.com>
2024-02-02 18:56:05 +0000

    merged both apis

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var apiKey = "0qKmN4G3IDKPTq3BC91TZXoTnlsKdCl0"
     
 var queryURL = "https://api.giphy.com/v1/gifs/random?tag=wine&api_key=" + "0qKmN4G3IDKPTq3BC91TZXoTnlsKdCl0";
@@ -10,3 +11,14 @@ var queryURL = "https://api.giphy.com/v1/gifs/random?tag=wine&api_key=" + "0qKmN
     //   var url = (data.images);
     //   console.log(url)
 });
+=======
+var apiKey = "d152576e58714ab5ba4e89d5f0607368"
+var queryURL = "https://api.spoonacular.com/recipes/complexSearch?query=pasta&apiKey=" + "d152576e58714ab5ba4e89d5f0607368";
+fetch(queryURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    });
+>>>>>>> 928b6fbc022777175d74008591fc4e4f94095588
