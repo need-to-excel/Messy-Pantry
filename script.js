5d6d39ce494b93a83fdafad818775b3ce9ada068
Hannah Hazel <hazelhannah48@gmail.com>
2024-02-07 19:06:07 +0000

    adding my js code

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ fetch(queryURL)
     })
     .then(function (data) {
         var mealdata = []
+        const WidgetContainer = document.querySelector(".widgets")
         for(index = 0; index < data.results.length; index++){
             // mealIds.push(data.results[index].id)
             var queryURL = "https://api.spoonacular.com/recipes/" + data.results[index].id + "/card?apiKey=" + "d152576e58714ab5ba4e89d5f0607368";
@@ -37,7 +38,6 @@ fetch(queryURL)
         }
         console.log(mealdata); 
         // console.log(data)  
-const WidgetContainer = document.querySelector(".widgets")
         for(index = 0; index < mealdata.length; index ++) {
             // WidgetContainer.innerHTML += `<img src="${mealdata[index]}" class="img-fluid" alt="...">`
         }
@@ -55,3 +55,35 @@ getMeal()
       console.log(mealInput)
       getMeal(mealInput);
       });
+
+function updateSearchHistory(mealInput) {
+  var searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+  searchHistory = searchHistory.slice(0, 8);
+  
+  {
+    name: mealInput.name
+  }
+
+  localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+
+  // Update the search history display on the website
+  var historyContainer = $('#history');
+  historyContainer.empty();
+  searchHistory.forEach(function (search) {
+    // Create a button element for each search history item
+    var historyItem = $('<button class="btn btn-secondary m-1 history-item">');
+    historyItem.text(search);
+    historyItem.on('click', function () {
+      updateMeal(search);
+    });
+    historyContainer.append(historyItem);
+  });
+
+}
+
+function updateMeal(mealInput) {
+  var currentMealURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+}
+
+// needs to be inside a function 
+//updateSearchHistory(mealInput);
